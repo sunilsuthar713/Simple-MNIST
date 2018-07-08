205d5800de8afebf6989db7030e8cafef3bbe6f4
sunilsuthar713 <sunilsuthar713@gmail.com>
2018-07-08 08:30:03 +0530

    Add files via upload

---
+++ SimpleMNIST/app/build/generated/source/buildConfig/debug/com/example/sunil/simplemnist/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.sunil.simplemnist;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.sunil.simplemnist";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
